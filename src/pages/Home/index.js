7a194611d681379db5515bc46bb90585f52e1764
vhrita <vih.trabalhos@gmail.com>
2022-06-14 18:00:11 -0300

    Homepage creation

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,20 +2,58 @@ import "./styles.sass"
 import ProfilePic from "../../images/profile.png"
 
 import { ConnectedLines } from "../../components/AnimatedBackgrounds"
+import { motion } from "framer-motion"
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faChevronCircleDown } from "@fortawesome/free-solid-svg-icons"
+
+const children = {
+    hidden: {opacity: 0},
+    show: { opacity: 1 }
+}
 
 export const Home = () => {
     return (
         <>
-            <main>
+            <motion.main
+                initial={{y: '+100vh'}}
+                animate={{y: 0}}
+                transition={{type: 'spring', duration: 1, bounce: 0, delayChildren: 1}}
+            >
                 <ConnectedLines />
-                <div className="bg-animation">
-                    <div className="texts">
+                <motion.div
+                    className="bg-animation"
+                    transition={{delayChildren: 1}}
+                >
+                    <motion.div 
+                        className="texts"
+                        variants={children}
+                        initial='hidden'
+                        animate='show'
+                        transition={{delay: 1, duration: 1.5}}
+                    >
                         <p>Olá, me chamo <strong>Vitor Rita!</strong></p>
                         <p>Segundo texto aqui</p>
-                    </div>
-                    <img alt="Vitor Rita" src={ProfilePic}></img>
-                </div>
-            </main>
+                    </motion.div>
+                    <motion.button
+                        variants={children}
+                        initial='hidden'
+                        animate='show'
+                        transition={{delay: 1.5, duration: 1.5}}
+                    >
+                        COMECE POR AQUI
+                        <FontAwesomeIcon icon={faChevronCircleDown}></FontAwesomeIcon>
+                    </motion.button>
+                    <motion.img
+                        alt="Vitor Rita"
+                        src={ProfilePic}
+                        variants={children}
+                        initial='hidden'
+                        animate='show'
+                        transition={{delay: 0.5, duration: 1.5}}
+                    />
+                </motion.div>
+            </motion.main>
         </>
     )
 }
