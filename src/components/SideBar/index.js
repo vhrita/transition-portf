26f121c4eaee489311190bd587273dd88c49213b
Vitor Rita <vitor.rita@paripassu.com.br>
2022-06-23 13:48:44 -0300

    Started codding hovered sidebar for mobile

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -3,11 +3,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faHome, faThList, faIdCardAlt } from '@fortawesome/free-solid-svg-icons'
 import { Link } from "react-router-dom"
 import { motion } from "framer-motion"
+import { isMobile } from "react-device-detect"
 
 export const SideBar = () => {
-    return (
-        <>
+    if(isMobile) {
+        return (
             <motion.nav
+                className="desktop"
                 initial={{x: '-50vw'}}
                 animate={{x: 0}}
                 transition={{type: 'spring', duration: 1, bounce: 0.2}}
@@ -26,6 +28,28 @@ export const SideBar = () => {
                     <span>Sobre</span>
                 </Link>
             </motion.nav>
-        </>
-    )
+        )   
+    } else {
+        return(
+            <motion.nav className="mobile">
+                <motion.aside>
+                    <Link to={'/'} className="nav-link">
+                        <FontAwesomeIcon icon={faHome} />
+                        <span>Home</span>
+                    </Link>
+                    <hr />
+                    <Link to={'/portfolio'} className="nav-link">
+                        <FontAwesomeIcon icon={faThList} />
+                        <span>Portfolio</span>
+                    </Link>
+                    <hr />
+                    <Link to={'/about'} className="nav-link">
+                        <FontAwesomeIcon icon={faIdCardAlt} />
+                        <span>Sobre</span>
+                    </Link>
+                    <hr />
+                </motion.aside>
+            </motion.nav>
+        )
+    }
 }
