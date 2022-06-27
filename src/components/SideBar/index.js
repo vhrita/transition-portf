0be86b016c78dfa24f2073c89049289c2d0697da
Vitor Rita <vitor.rita@paripassu.com.br>
2022-06-26 23:44:38 -0300

    Starded nav side hovered bar

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,9 +1,39 @@
 import "./styles.sass"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faHome, faThList, faIdCardAlt } from '@fortawesome/free-solid-svg-icons'
+import { faHome, faThList, faIdCardAlt, faBars } from '@fortawesome/free-solid-svg-icons'
 import { Link } from "react-router-dom"
 import { motion } from "framer-motion"
 import { isMobile } from "react-device-detect"
+
+const paths = [
+    {
+        "path" : "/",
+        "name" : "Home",
+        "icon": faHome
+    },
+    {
+        "path" : "/portfolio",
+        "name" : "Portfolio",
+        "icon": faThList
+    },
+    {
+        "path" : "/about",
+        "name" : "Sobre",
+        "icon": faIdCardAlt
+    }
+]
+
+const LinkConstructor = (link) => {
+        return (
+            <>
+                <Link to={link.path} className="nav-link">
+                    <FontAwesomeIcon icon={link.icon} />
+                    <span>{link.name}</span>
+                </Link>  
+                {!isMobile && <hr />}
+            </>
+        )
+}
 
 export const SideBar = () => {
     if(isMobile) {
@@ -15,41 +45,37 @@ export const SideBar = () => {
                 transition={{type: 'spring', duration: 1, bounce: 0.2}}
                 style={{y: '-50%'}}
             >
-                <Link to={'/'} className="nav-link">
-                    <FontAwesomeIcon icon={faHome} />
-                    <span>Home</span>
-                </Link>
-                <Link to={'/portfolio'} className="nav-link">
-                    <FontAwesomeIcon icon={faThList} />
-                    <span>Portfolio</span>
-                </Link>
-                <Link to={'/about'} className="nav-link">
-                    <FontAwesomeIcon icon={faIdCardAlt} />
-                    <span>Sobre</span>
-                </Link>
+                {
+                    paths.map((link) => {
+                        return <linkConstructor link={link} key={link.path}  />
+                    })
+                }
             </motion.nav>
         )   
     } else {
         return(
-            <motion.nav className="mobile">
-                <motion.aside>
-                    <Link to={'/'} className="nav-link">
-                        <FontAwesomeIcon icon={faHome} />
-                        <span>Home</span>
-                    </Link>
-                    <hr />
-                    <Link to={'/portfolio'} className="nav-link">
-                        <FontAwesomeIcon icon={faThList} />
-                        <span>Portfolio</span>
-                    </Link>
-                    <hr />
-                    <Link to={'/about'} className="nav-link">
-                        <FontAwesomeIcon icon={faIdCardAlt} />
-                        <span>Sobre</span>
-                    </Link>
-                    <hr />
-                </motion.aside>
-            </motion.nav>
+            <>
+                <motion.button
+                    initial={{x: '-50vw'}}
+                    animate={{x: '1vw'}}
+                    transition={{type: 'spring', duration: 1, bounce: 0.2}}
+                    className='collapse-mobile'
+                >
+                    <FontAwesomeIcon icon={faBars} />
+                </motion.button>
+                <motion.nav
+                    className="mobile"
+                    initial={{visibility: 'hidden', opacity: 0}}
+                >
+                    <motion.aside>
+                    {
+                        paths.map((link) => {
+                            return <linkConstructor link={link} key={link.path}  />
+                        })
+                    }
+                    </motion.aside>
+                </motion.nav>
+            </>
         )
     }
 }
