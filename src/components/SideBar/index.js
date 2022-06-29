e7fd4eef5dba307a293cdf36613ef5b85a09fd8c
vhrita <vih.trabalhos@gmail.com>
2022-06-29 13:15:29 -0300

    NavBar responsiva

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,53 +1,63 @@
 import "./styles.sass"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faHome, faThList, faIdCardAlt, faBars } from '@fortawesome/free-solid-svg-icons'
-import { Link } from "react-router-dom"
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { motion } from "framer-motion"
 import { isMobile } from "react-device-detect"
+import { NavLink } from "./components/NavLink"
+import { useEffect, useState } from "react"
+import { useLocation } from "react-router-dom"
 
 const paths = [
     {
         "path" : "/",
         "name" : "Home",
-        "icon": faHome
+        "icon": "home"
     },
     {
         "path" : "/portfolio",
         "name" : "Portfolio",
-        "icon": faThList
+        "icon": "th-list"
     },
     {
         "path" : "/about",
         "name" : "Sobre",
-        "icon": faIdCardAlt
+        "icon": "id-card-alt"
     }
 ]
 
-const LinkConstructor = (link) => {
-        return (
-            <>
-                <Link to={link.path} className="nav-link">
-                    <FontAwesomeIcon icon={link.icon} />
-                    <span>{link.name}</span>
-                </Link>  
-                {!isMobile && <hr />}
-            </>
-        )
-}
-
 export const SideBar = () => {
-    if(isMobile) {
+    const [isOpen, setIsOpen] = useState(false)
+    const [onTop, setOnTop] = useState(false)
+
+    const location = useLocation()
+
+    useEffect(() => {
+        location.pathname === '/portfolio'
+        ? setOnTop(true)
+        : setOnTop(false)
+    }, [location])
+
+    if(!isMobile) {
         return (
             <motion.nav
-                className="desktop"
-                initial={{x: '-50vw'}}
-                animate={{x: 0}}
-                transition={{type: 'spring', duration: 1, bounce: 0.2}}
-                style={{y: '-50%'}}
+                className={onTop ? 'dsktp-top' : 'dsktp-left'}
+                initial={{
+                    X: !onTop && '-50vw',
+                    y: onTop && '-50vh'
+                }}
+                animate={{
+                    x: !onTop ? 0 : undefined,
+                    y: onTop ? 0 : undefined
+                }}
+                transition={{type: 'spring', duration: 1, bounce: 0.3}}
+                style={{
+                    x: onTop ? '-50%' : '50%',
+                    y: !onTop ? '-50%' : '50%'
+                }}
             >
                 {
-                    paths.map((link) => {
-                        return <linkConstructor link={link} key={link.path}  />
+                    paths?.map((link) => {
+                        return <NavLink key={link.path} link={link} isMobile={false}  />
                     })
                 }
             </motion.nav>
@@ -56,21 +66,35 @@ export const SideBar = () => {
         return(
             <>
                 <motion.button
-                    initial={{x: '-50vw'}}
-                    animate={{x: '1vw'}}
+                    initial={{x: '-50vw', visibility: 'hidden'}}
+                    animate={{
+                        visibility: isOpen ? 'hidden' : 'visible',
+                        x: isOpen ? '-50vw' : '1vw'
+                    }}
                     transition={{type: 'spring', duration: 1, bounce: 0.2}}
                     className='collapse-mobile'
+                    onClick={() => { setIsOpen(true) }}
                 >
                     <FontAwesomeIcon icon={faBars} />
                 </motion.button>
                 <motion.nav
                     className="mobile"
                     initial={{visibility: 'hidden', opacity: 0}}
+                    animate={{
+                        visibility: isOpen ? 'visible' : 'hidden',
+                        opacity: isOpen ? 1 : 0
+                    }}
+                    transition={{duration: 0.5, delay: isOpen ? 0 : 0.5}}
+                    onClick={() => { setIsOpen(false) }}
                 >
-                    <motion.aside>
+                    <motion.aside
+                        initial={{x: '-50vw'}}
+                        animate={{x: isOpen ? '0vw' : '-100vw'}}
+                        transition={{type: 'spring', duration: 1, bounce: 0.2}}
+                    >
                     {
-                        paths.map((link) => {
-                            return <linkConstructor link={link} key={link.path}  />
+                        paths?.map((link) => {
+                            return <NavLink key={link.path} link={link} isMobile={true}  />
                         })
                     }
                     </motion.aside>
