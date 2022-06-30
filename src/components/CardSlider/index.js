d51967e30b900046c7a443eada0b544f6bd491b5
vhrita <vih.trabalhos@gmail.com>
2022-06-30 04:04:39 -0300

    Responsive and beautiful CardSlider ready

--- src/components/CardSlider/index.js
+++ src/components/CardSlider/index.js
@@ -1,15 +1,64 @@
 import "./styles.sass"
+import { useState } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons"
+import { motion } from "framer-motion"
+import { useSwipeable } from "react-swipeable"
+import { isMobile } from "react-device-detect"
 
-export const CardSlider = () => {
+export const CardSlider = ({ cards }) => {
+    const [ position, setPosition ] = useState(0)
+    
+    const onRight = () => {
+        position<cards.length-1
+        ? setPosition(position+1)
+        : setPosition(0)
+    }
+    const onLeft = () => {
+        position>0
+        && setPosition(position-1)
+    }
+    const swipeHandler = useSwipeable({
+        onSwipedRight: onLeft,
+        onSwipedLeft: onRight,
+    })
+
     return (
-        <div className="slider-deck">
-            <FontAwesomeIcon icon={faChevronLeft} className="deck-btn" />
-            <div className="secondary"></div>
-            <div className="main"></div>
-            <div className="secondary"></div>
-            <FontAwesomeIcon icon={faChevronRight} className="deck-btn" />
+        <div {...swipeHandler} className="slider-deck">
+            {
+                cards.map((url, index) => (
+                    <motion.div
+                        className="card"
+                        key={index}
+                        initial={{ scale: 0, rotation: -180 }}
+                        animate={{
+                            rotate: 0,
+                            left: `${(index-position) * 70 + 5}vw`,
+                            scale: index === position ? 1 : 0.8
+                        }}
+                        transition={{
+                            type: 'spring',
+                            stiffness: '260',
+                            damping: '20'
+                        }}
+                    >
+                        <img src={url} alt="project" />
+                    </motion.div>
+                ))
+            }
+            {!isMobile &&
+                <div className="controls">
+                    <button type="button" disabled={position<=0} onClick={onLeft}>
+                        <FontAwesomeIcon icon={faChevronLeft} />
+                    </button>
+                    <span>{position+1}/{cards.length}</span>
+                    <button type="button" disabled={false} onClick={onRight}>
+                        <FontAwesomeIcon icon={faChevronRight} />
+                    </button>
+                </div>
+            }
+            
+            {isMobile && <span>{position+1}/{cards.length}</span>}
         </div>
     )
 }
--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -5,7 +5,13 @@ export const Portfolio = () => {
     return (
         <main>
             <div className="portfolio">
-                <CardSlider />
+                <CardSlider cards={[
+                    'https://i.picsum.photos/id/985/1920/1080.jpg?hmac=dLb_5lBJW4AZhZDUXweY5WlrnscpKLtCKffaqSrF9Vw',
+                    'https://i.picsum.photos/id/404/1920/1080.jpg?hmac=5ulPfhEdiQ1oAWu_mMT6A_wKqZVyLKpD7zWZmgNgNp8',
+                    'https://i.picsum.photos/id/520/1920/1080.jpg?hmac=XYsU8XvBEUmWVcm-mX92ro1smqwEIiRZNCFDaNXDXj8',
+                    'https://i.picsum.photos/id/570/1920/1080.jpg?hmac=GrPj9g_cV2WHQPt582h1bKvbTSRzDejP6FOf7P20Q8Y',
+                    'https://i.picsum.photos/id/405/1920/1080.jpg?hmac=qXJMvqqkP1QYgTezWaA03FiDMAH9rr6E9U4LZwLVRbk'
+                ]} />
             </div>
         </main>
     )
