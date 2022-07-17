124c8fbfac391f99f67288afc61e4f4fa137ae09
vhrita <vih.trabalhos@gmail.com>
2022-07-17 20:48:07 -0300

    homepage and .env

--- src/lib/apollo.js
+++ src/lib/apollo.js
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl510ozy31aou01uegz9tco28/master',
+    uri: process.env.REACT_APP_DB_URI,
     cache: new InMemoryCache()
 })
