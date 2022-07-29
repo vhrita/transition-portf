7ab07300d7bf79d219269c47551b75cde1b32c77
Vitor Rita <vitor.rita@paripassu.com.br>
2022-07-29 11:00:36 -0300

    Coming Soon element added

--- src/components/AnimatedRoutes/index.js
+++ src/components/AnimatedRoutes/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Home } from "../../pages/Home"
 import { Portfolio } from "../../pages/Portfolio"
+import { ComingSoon } from "../../pages/Soon"
 import { Route, Routes, useLocation } from "react-router-dom"
 
 import {AnimatePresence} from "framer-motion"
@@ -13,6 +14,7 @@ export const AnimatedRoutes = () => {
             <Routes location={location} key={location.pathname}>
                 <Route path="/" exact element={<Home />} />
                 <Route path="/portfolio" element={<Portfolio />} />
+                <Route path="/about" element={<ComingSoon />} />
             </Routes>
         </AnimatePresence>
     )
